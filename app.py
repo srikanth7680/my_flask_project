afc7ae99d7d1aa9918f53895ed7255b4dd51de69
Srikanth7680 <srikanth123ppoola@gmail.com>
2025-10-24 23:06:15 +0530

    to-do-app

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flask!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, FlaskKKKKK!"
+# Temporary list (we'll add DB later)
+tasks = []
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/create', methods=['POST'])
+def create():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:task_id>', methods=['POST'])
+def update(task_id):
+    task = request.form.get('task')
+    if task:
+        tasks[task_id] = task
+    return redirect(url_for('index'))   
+
+if __name__ == '__main__':
     app.run(debug=True)
